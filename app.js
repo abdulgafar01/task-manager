c1c45a72b2365704affbbbe7fd8438765f2916b5
abdulgafar <abdulgafarbusari49@gmail.com>
2025-04-10 14:12:03 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+
+
+
+
+app.get('/', (req, res)=> {
+    res.send('welcome')
+})
+
+
+app.use('/api/v1/tasks/', tasks)
+
+const port = 3000
+app.listen(port, console.log(`server is listening on ${port}`))
+ 
---
+++ controllers/tasks.js
@@ -0,0 +1,24 @@
+const getAllTasks = (req, res)=>{
+    res.send("get all the task")
+}
+const createTask = (req, res)=>{
+    res.send("create  a single task")
+}
+const getTask = (req, res)=>{
+    res.send("get a single task")
+}
+const updateTask = (req, res)=>{
+    res.send("update task")
+}
+const deleteTask = (req, res)=>{
+    res.send("delete task")
+}
+
+
+module.exports = {
+    getAllTasks, 
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+}
